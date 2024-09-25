4aaee1fb250c3ed4d337bf641afdfe876f9c6ecb
Freddec <freddec.games@gmail.com>
2024-09-25 17:39:32 +0200

    initial

---
+++ .output/public/_nuxt/DvEnWBtC.js
@@ -0,0 +1,1 @@
+import{m as e}from"./EBHw3XF1.js";const n={__name:"index",setup(r){return e().push("/game"),(u,o)=>null}};export{n as default};
---
+++ .output/server/chunks/build/client.manifest.mjs
@@ -0,0 +1,189 @@
+const client_manifest = {
+  "_C1xH3N-8.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C1xH3N-8.js",
+    "name": "utils"
+  },
+  "_CcbLNUag.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CcbLNUag.js",
+    "name": "ModalWipe",
+    "imports": [
+      "_DlAUqK2U.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DKqwsp4J.js"
+    ]
+  },
+  "_DKqwsp4J.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DKqwsp4J.js",
+    "name": "appStore",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_DlAUqK2U.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DlAUqK2U.js",
+    "name": "_plugin-vue_export-helper"
+  },
+  "_ql_KRCr6.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "ql_KRCr6.js",
+    "name": "nuxt-link",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "layouts/home.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "jCUqzyjI.js",
+    "name": "home",
+    "src": "layouts/home.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_C1xH3N-8.js",
+      "_DKqwsp4J.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_ql_KRCr6.js",
+      "_CcbLNUag.js",
+      "_DlAUqK2U.js"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Cu8b7YnH.js",
+    "name": "error-404",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_ql_KRCr6.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "error-404.JekaaCis.css"
+    ]
+  },
+  "error-404.JekaaCis.css": {
+    "file": "error-404.JekaaCis.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Dj-YutSI.js",
+    "name": "error-500",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DlAUqK2U.js"
+    ],
+    "css": [
+      "error-500.CNP9nqm1.css"
+    ]
+  },
+  "error-500.CNP9nqm1.css": {
+    "file": "error-500.CNP9nqm1.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "EBHw3XF1.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "layouts/home.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.BP4nr13t.css"
+    ]
+  },
+  "entry.BP4nr13t.css": {
+    "file": "entry.BP4nr13t.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/game.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Dmd2zgXq.js",
+    "name": "game",
+    "src": "pages/game.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_C1xH3N-8.js",
+      "_DKqwsp4J.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DvEnWBtC.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "pages/options.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "KAHCR8gG.js",
+    "name": "options",
+    "src": "pages/options.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_CcbLNUag.js",
+      "_DlAUqK2U.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_DKqwsp4J.js"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
