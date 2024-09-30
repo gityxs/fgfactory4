2e238366dc516cdbd27868e6805c7661d8b251a9
Freddec <freddec.games@gmail.com>
2024-09-30 09:00:16 +0200

    Updates

--- _nuxt/BX6nokE7.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./RU1EJBCX.js";const n={__name:"index",setup(r){return e().push("/game"),(u,o)=>null}};export{n as default};
---
+++ _nuxt/CQDZG7s-.js
@@ -0,0 +1,1 @@
+import{m as e}from"./D3iobiq4.js";const n={__name:"index",setup(r){return e().push("/game"),(u,o)=>null}};export{n as default};
