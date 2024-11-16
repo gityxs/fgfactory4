477fd3fdd2bc3f8d2e58653832b95a00b51ea060
Freddec <freddec.games@gmail.com>
2024-11-16 09:55:41 +0100

    Updates

---
+++ _nuxt/B2zjzzwx.js
@@ -0,0 +1,1 @@
+import{s}from"./BtZ4-IfP.js";const t=s("/victory.png");export{t as _};
---
+++ _nuxt/CuO8sO4u.js
@@ -0,0 +1,1 @@
+import{k as e}from"./BtZ4-IfP.js";const n={__name:"index",setup(r){return e().push("/milestones"),(s,o)=>null}};export{n as default};
