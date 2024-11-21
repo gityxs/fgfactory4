5a0e6b3bd1a2f05c2f6480c0502923275bff4e17
Freddec <freddec.games@gmail.com>
2024-11-21 11:21:03 +0100

    Updates

---
+++ _nuxt/C_ABXYvq.js
@@ -0,0 +1,1 @@
+import{e as c,f as r,o as m,c as p,t as u,h as t,m as i,p as l}from"./BxDCtSri.js";const f={__name:"ItemCount",props:["id"],setup(a){const s=a,n=c(),e=r(()=>n.elems.find(o=>o.id==s.id));return(o,_)=>(m(),p("span",{class:l({"opacity-50":t(e).count==0,"text-white":t(e).count>0&&t(e).count<t(e).max,"text-danger":t(e).count>=t(e).max})},u(t(i)(t(e).count,"",0)),3))}};export{f as _};
---
+++ _nuxt/CtmY_hQ7.js
@@ -0,0 +1,1 @@
+import{Y as s}from"./BxDCtSri.js";const p=s("/patreon.png"),t=s("/kofi.png"),a=s("/paypal.png"),n=s("/discord.png");export{p as _,t as a,a as b,n as c};
